86d80b240fdd627ebc10775db6ac30bf21a6467f
hevidar.tekinn <hevidar.tekinn@gmail.com>
2025-09-09 13:22:11 +0300

    Projedeki değişiklik: [yorum satırlarını sildim]

--- Deneme/Program.cs
+++ Deneme/Program.cs
@@ -6,7 +6,6 @@ using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<BlogDbContext>(options =>
@@ -26,11 +25,10 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    
     app.UseHsts();
 }
 
